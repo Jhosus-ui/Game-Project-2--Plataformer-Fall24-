ddd338422534de185c9fb403380a557530b99314
Jhosus-ui <jhosu.uchiha@gmail.com>
2024-12-04 23:50:09 -0500

    Guardar

--- Assets/Scripts 1/Camera Shake.cs
+++ Assets/Scripts 1/Camera Shake.cs
@@ -1,18 +1,37 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
-public class CameraShake : MonoBehaviour
+public class CameraShakeCinemachine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CinemachineBasicMultiChannelPerlin noise; // Referencia al componente de ruido
+    private CinemachineVirtualCamera virtualCamera; // Referencia a la cámara virtual
+
+    private void Start()
     {
-        
+        // Obtener la cámara virtual
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+
+        // Obtener el componente de ruido
+        if (virtualCamera != null)
+        {
+            noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Shake(float duration, float amplitude, float frequency)
     {
-        
+        if (noise != null)
+        {
+            // Configurar la intensidad y frecuencia del temblor
+            noise.m_AmplitudeGain = amplitude;
+            noise.m_FrequencyGain = frequency;
+
+            yield return new WaitForSeconds(duration);
+
+            // Restaurar los valores a 0 después del temblor
+            noise.m_AmplitudeGain = 0f;
+            noise.m_FrequencyGain = 0f;
+        }
     }
 }
